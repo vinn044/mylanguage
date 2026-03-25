302bd9605a96d56eb9e039fe6cb7da760c575d22
vin <vincek1800@gmail.com>
2026-03-25 03:13:00 -0500

    item support done

--- RPG.hs
+++ RPG.hs
@@ -5,6 +5,10 @@ data Command =
     Move Direction
   | Dodge Direction
   | Grab Item
+  | Drop Item
+  | Use Item
+  | Equip Item
+  | Unequip Item
   deriving (Show)
 
 data Item = Potion | Sword | Armor 
