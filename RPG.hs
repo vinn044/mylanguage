9f75f2082ebd8332b13ca6b6e9640312f2c89fb2
vin <vincek1800@gmail.com>
2026-04-18 23:12:47 -0500

    fixes (removed cons, and dodge)

--- RPG.hs
+++ RPG.hs
@@ -1,44 +1,54 @@
 module RPG where
 import Data.List (intercalate)
+
 data Direction = LeftDir | RightDir | UpDir | DownDir
-  deriving (Show)
+
+instance Show Direction where
+  show LeftDir = "left"
+  show RightDir = "right"
+  show UpDir = "up"
+  show DownDir = "down"
+
 
 data AttackType = Overhand | Slash
-  deriving (Show)
+
+instance Show AttackType where
+  show Overhand = "overhand"
+  show Slash = "slash"
 
 
 data Item = Potion | Sword | Armor 
   deriving (Show)
 
+
 data Command = Move Int
-  | Dodge Direction
+  | Turn Direction
   | Grab Item
   | Drop Item
   | Use Item
   | Equip Item
   | Unequip Item
   | Attack AttackType
- -- deriving (Show)
+
 instance Show Command where
   show (Move i) = "move: " ++ show i
-  show (Dodge dir) = "dodge: " ++ show dir
-  show _ = undefined
+  show (Turn dir) = "turn: " ++ show dir
+  show (Grab item) = "grab: " ++ show item
+  show (Drop item) = "drop: " ++ show item
+  show (Use item) = "use: " ++ show item
+  show (Equip item) = "equip: " ++ show item
+  show (Unequip item) = "unequip: " ++ show item
+  show (Attack atk) = "attack: " ++ show atk
 
-data CommandList
-  = Single Command
-  | Cons Command CommandList
+
+-- CHANGED: now using built-in list instead of custom recursion
+data Program = Program Decl [Command]
   deriving (Show)
 
--- data Maybe a = Nothing | Just a
-convert :: [Command] -> CommandList
-convert [c] = Single c
-convert (c : cs) = Cons c (convert cs)
-
-data Program = Program Decl CommandList
-  deriving (Show)
 
 type Location = (Int, Int)
 type Decl = (GameBoard, Player)
+
 
 data Player = Player {
   name :: String,
@@ -49,12 +59,14 @@ data Player = Player {
 }
 
 instance Show Player where
-  show (Player n items Nothing dir pos) = show n ++ ":\n"
-          ++ "items: {" ++ intercalate ", " (map show items) ++ "}" ++ "\n"
+  show (Player n items Nothing dir pos) =
+          n ++ ":\n"
+          ++ "items: {" ++ intercalate ", " (map show items) ++ "}\n"
           ++ "direction: " ++ show dir ++ "\n"
           ++ "position: " ++ show pos
-  show (Player n items (Just itm) dir pos) = show n ++ ":\n"
-          ++ "items: {" ++ intercalate ", " (map show items) ++ "}" ++ "\n"
+  show (Player n items (Just itm) dir pos) =
+          n ++ ":\n"
+          ++ "items: {" ++ intercalate ", " (map show items) ++ "}\n"
           ++ "equipped: " ++ show itm ++ "\n"
           ++ "direction: " ++ show dir ++ "\n"
           ++ "position: " ++ show pos
@@ -66,13 +78,17 @@ data GameBoard = GameBoard {
   items :: [(Location, Item)]
 }
 
+
 data Env = Env {
   player :: Player,
   board :: GameBoard
 }
 
+
 initialPlayer = Player "Vince" [] Nothing UpDir (0,0)
+
 initialBoard = GameBoard 10 5 [((2,3), Sword), ((1,1), Potion), ((4,6), Sword)]
+
 decl = (initialBoard, initialPlayer)
 
-p1 = Program decl (convert [Move 3, Dodge LeftDir])
+p1 = Program decl [Move 3, Turn LeftDir]
